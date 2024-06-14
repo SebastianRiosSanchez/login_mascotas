2345566ea5956105e1bbecd194257a58b49aef0c
SebastianRiosSanchez <sebastianrios47@gmail.com>
2024-06-14 08:59:11 -0500

    implementacion en el componente customer de los metodos del servicio customersService

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from 'src/app/services/customers/customers.service';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customerService: CustomersService) { }
 
   ngOnInit(): void {
+    //Llamado al método para cargar los datos
+    this.loadInfo();
   }
+
+  //Método en cargado de traer los datos para llenar la tabla con los registros de clientes en la base de datos
+  loadInfo() {
+    this.customerService.getCustomers().subscribe(
+      {
+        next: (response) => {
+          console.log('Lista de clientes: ', response);
+        },
+        error: (errorData) => {
+          console.error(errorData);
+        },
+        complete: () => {
+          console.log('Completado Clientes');
+        }
+      }
+    );
+  }
+
 
 }
